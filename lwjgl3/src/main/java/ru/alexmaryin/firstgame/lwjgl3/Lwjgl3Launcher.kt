691feea2da50d90516fb3d6d08c2953addbb57d3
alexmaryin <java73@yandex.ru>
2021-06-08 22:13:06 +0400

    Classes converted to Kotlin

--- lwjgl3/src/main/java/ru/alexmaryin/firstgame/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/java/ru/alexmaryin/firstgame/lwjgl3/Lwjgl3Launcher.kt
@@ -5,22 +5,20 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import ru.alexmaryin.firstgame.StartWindow
 
 /** Launches the desktop (LWJGL3) application.  */
-object Lwjgl3Launcher {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        createApplication()
-    }
 
-    private fun createApplication(): Lwjgl3Application {
-        return Lwjgl3Application(StartWindow(), defaultConfiguration)
-    }
-
-    private val defaultConfiguration: Lwjgl3ApplicationConfiguration
-        get() {
-            val configuration = Lwjgl3ApplicationConfiguration()
-            configuration.setTitle("firstGame")
-            configuration.setWindowedMode(640, 480)
-            configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
-            return configuration
-        }
+fun main(args: Array<String>) {
+    createApplication()
 }
+
+private fun createApplication(): Lwjgl3Application {
+    return Lwjgl3Application(StartWindow(), defaultConfiguration)
+}
+
+private val defaultConfiguration: Lwjgl3ApplicationConfiguration
+    get() {
+        val configuration = Lwjgl3ApplicationConfiguration()
+        configuration.setTitle("firstGame")
+        configuration.setWindowedMode(640, 480)
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
+        return configuration
+    }
